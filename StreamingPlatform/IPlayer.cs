dc26f14ec2b41aaa759d4421e26ee1cd68b3d357
Alex <alex.sircu@gmail.com>
2023-04-21 10:07:22 +0200

    add StreamingPlatform project

---
+++ StreamingPlatform/IPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamingMedia
+{
+    internal interface IPlayer
+    {
+        public void Play(int SongId);
+        public void ShowPlaying();
+        public void StartPlaying();
+        public void Stop();
+        public void Pause();
+        public void Rate();
+        public void Forward();
+        public void Backward();
+        public void MediaList();
+    }
+}
