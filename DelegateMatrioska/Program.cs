87ec392162b7b7630ef20d45d9e58ff60dcb2623
Alex <alex.sircu@gmail.com>
2023-04-27 12:50:46 +0200

    add DelegateMatrioska project

---
+++ DelegateMatrioska/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static DelegateMatrioska.Program;
+
+namespace DelegateMatrioska
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionClass functionClass = new FunctionClass();
+
+            //delegate che esegue metodo Sum
+            SumDelegate sumDelegate = functionClass.Sum;
+
+            //delegate che esegue metodo ExecuteSumDelegate
+            ExecuteDelegate executeDelegate = functionClass.ExecuteSumDelegate;
+
+            //metodo che esegue tutto quanto
+            functionClass.Execute(executeDelegate, sumDelegate);
+
+        }
+
+        public delegate void ExecuteDelegate(SumDelegate sumDelegate);
+        public delegate void SumDelegate(int x, int y);        
+
+        public class FunctionClass
+        {
+            public void Sum(int x, int y)
+            {
+                Console.WriteLine(x+y);
+            }
+
+            public void ExecuteSumDelegate(SumDelegate sumDelegate)
+            {
+                sumDelegate(12, 45);
+            }
+
+            public void Execute(ExecuteDelegate executeDelegate, SumDelegate sumDelegate)
+            {
+                executeDelegate(sumDelegate);
+            }
+        }
+    }
+}
